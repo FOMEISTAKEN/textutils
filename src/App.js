15aae6f56f0d4e1e8a893b6a463bb34d32038275
Fomer Clarckson <farhanisawake@gmail.com>
2022-01-08 18:02:30 +0600

    react-router-dom installed

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import React from 'react'
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App() {
   return (
-<>
+<div>
     <Navbar title="RJC" aboutText="About RJC"/>
 <div className="container">
-    {/* <TextForm name="Your Text" heading="Enter Your text"/> */}
+    <TextForm name="Your Text" heading="Enter Your text"/>
     <About/>
+
 </div>
-</>
+</div>
   );
 }
 
