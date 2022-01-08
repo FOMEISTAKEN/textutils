b073cb6bf4092e0d02ef3a47e7d9095530e21204
Fomer Clarckson <farhanisawake@gmail.com>
2022-01-08 17:38:56 +0600

    added dark mode

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import React from 'react'
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
 
 function App() {
   return (
 <>
     <Navbar title="RJC" aboutText="About RJC"/>
 <div className="container">
-    <TextForm name="Your Text" heading="Enter Your text"/>
+    {/* <TextForm name="Your Text" heading="Enter Your text"/> */}
+    <About/>
 </div>
 </>
   );
